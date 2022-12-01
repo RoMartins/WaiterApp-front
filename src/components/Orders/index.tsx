17b6a53e8917ec7e70e93c5e750067e013a1526d
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-12-01 00:21:02 -0300

    Component order

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,8 +1,70 @@
-import {} from './styles';
+import {Content,Board, OrdersContainer} from './styles';
 
 
 export function Orders() {
-  return  <h1>Orders</h1>;
+  return (
+    <Content>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 02</strong>
+            <span>3 itens</span>
+          </button>
+
+          <button type='button'>
+            <span>Mesa 02</span>
+            <span>3 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 02</strong>
+            <span>3 itens</span>
+          </button>
+
+          <button type='button'>
+            <span>Mesa 02</span>
+            <span>3 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type='button'>
+            <strong>Mesa 02</strong>
+            <span>3 itens</span>
+          </button>
+
+          <button type='button'>
+            <span>Mesa 02</span>
+            <span>3 itens</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </Content>
+  );
 }
 
 
--- src/components/Orders/styles.ts
+++ src/components/Orders/styles.ts
@@ -1,1 +1,64 @@
 import styled from 'styled-components';
+
+export const Content = styled.div`
+width: 100%;
+max-width: 1216px;
+margin: 2.5rem auto;
+display: flex;
+gap: 2rem;
+`;
+
+
+export const Board = styled.div`
+  padding: 1rem;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+
+ header {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  font-size: 0.875rem;
+  margin-bottom: 1.5rem;
+ }
+`;
+
+
+
+export const OrdersContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+ button {
+  background: #FFFFFF;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 8px;
+  padding: 1rem;
+  height: 128px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4px  ;
+
+  strong {
+    font-weight: 500;
+  }
+
+  span {
+    color: #666;
+    font-size: 0.875rem;
+  }
+
+  & + button {
+    margin-top: 24px;
+  }
+
+}
+`;
